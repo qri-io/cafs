81556cae36f3ade7ba889b393a0c63f1a4474f00
b5 <sparkle_pony_2000@qri.io>
2018-09-06 11:57:13 -0400

    feat(migration error): detect ipfs migration errors
    Unfortunatley we have to deal with IPFS repo migrations. Rather than bloat our own
    codebase, I think we should send users over to download IPFS & update manually. I
    know it's a pain, but at least we provide a meaningful message to get un-stuck,
    and don't have to take on the task of supporting migrations in-process quite yet.

--- ipfs/config.go
+++ ipfs/config.go
@@ -8,6 +8,16 @@ import (
 	fsrepo "gx/ipfs/QmebqVUQQqQFhg74FtQFszUJo22Vpr3e8qBAkvvV4ho9HH/go-ipfs/repo/fsrepo"
 )
 
+var ErrIPFSRepoNeedsMigration = fmt.Errorf(`Your IPFS repo needs an update!
+
+Please make sure you have the latest version of IPFS (go-ipfs, at least v0.4.17)
+from either https://dist.ipfs.io or your favourite package manager. 
+
+Then run 'ipfs daemon' from a terminal. It should ask if you'd like to perform
+a migration, select yes. 
+
+Once you've migrated, run Qri connect again.`)
+
 // StoreCfg configures the datastore
 type StoreCfg struct {
 	// embed options for creating a node
@@ -42,6 +52,9 @@ func (cfg *StoreCfg) InitRepo() error {
 	if cfg.FsRepoPath != "" {
 		localRepo, err := fsrepo.Open(cfg.FsRepoPath)
 		if err != nil {
+			if err == fsrepo.ErrNeedMigration {
+				return ErrIPFSRepoNeedsMigration
+			}
 			return fmt.Errorf("error opening local filestore ipfs repository: %s\n", err.Error())
 		}
 		cfg.Repo = localRepo
